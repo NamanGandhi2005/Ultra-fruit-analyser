2d5654ca52c1244a5c96ff1cb2c5a5a0bd3b1b6d
Naman Gandhi <namangandhipersonal@gmail.com>
2026-04-26 21:47:58 +0530

    Fix missing import and rebuild app

--- fruit_analyzer_mobile/lib/screens/settings_screen.dart
+++ fruit_analyzer_mobile/lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
